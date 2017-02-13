12aade702ec88580f69df4556d68ffc63472aa1e
Buganini Q <buganini@Buganinis-Mac-mini.local>
2017-02-13 17:03:53 +0800

    Initial Commit

---
+++ whereim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whereim
+//
+//  Created by Buganini Q on 13/02/2017.
+//  Copyright © 2017 Where.IM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
